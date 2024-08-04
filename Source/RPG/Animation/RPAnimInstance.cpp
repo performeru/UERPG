dc7d3c607d5eee77c46d08c84b4f1348b8d852d6
performeru <performeru@naver.com>
2024-08-04 23:40:22 +0900

    Invalid file commitment

--- Source/RPG/Animation/RPAnimInstance.cpp
+++ Source/RPG/Animation/RPAnimInstance.cpp
@@ -2,4 +2,36 @@
 
 
 #include "Animation/RPAnimInstance.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
+URPAnimInstance::URPAnimInstance()
+{
+	MovingThreshould = 3.0f;
+	JumpingThreshould = 100.0f;
+}
+
+void URPAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	Owner = Cast<ACharacter>(GetOwningActor());
+	if (Owner)
+	{
+		Movement = Owner->GetCharacterMovement();
+	}
+}
+
+void URPAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (Movement)
+	{
+		Velocity = Movement->Velocity;
+		GroundSpeed = Velocity.Size2D();
+		bIsIdle = GroundSpeed < MovingThreshould;
+		bIsFalling = Movement->IsFalling();
+		bIsJumping = bIsFalling & (Velocity.Z > JumpingThreshould);
+	}
+}
